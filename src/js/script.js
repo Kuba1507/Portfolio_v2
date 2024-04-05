50efdc2654dffd8a658b8f5839bb71f86184fd6e
Kuba1507 <kuba.romanski@onet.com.pl>
2024-04-05 11:57:08 +0200

    styled and scripted burger btn

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+const burgerBtn = document.querySelector(".hamburger");
+const mobileNav = document.querySelector(".mobile-nav");
+const mobileNavItems = document.querySelectorAll(".mobile-nav__menu-item");
+
+const handleNav = () => {
+	mobileNav.classList.toggle("show-mobile-nav");
+	handleBurgerAnimation();
+	handleNavItemsAnimation();
+	deleteAnimation();
+};
+
+const handleBurgerAnimation = () => {
+	burgerBtn.classList.toggle("is-active");
+};
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	mobileNavItems.forEach((item) => {
+		item.classList.toggle("nav-items-animation");
+		item.style.animationDelay = "." + delayTime + "s";
+		delayTime++;
+	});
+};
+
+const deleteAnimation = () => {
+	mobileNavItems.forEach((item) => {
+		item.addEventListener("click", () => {
+			mobileNavItems.forEach((el) => {
+				el.classList.remove("nav-items-animation");
+			});
+		});
+	});
+};
+
+burgerBtn.addEventListener("click", handleNav);
