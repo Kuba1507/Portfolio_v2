1e7feb8075d4d184f7632079d6ee7f79477682bf
Kuba1507 <kuba.romanski@onet.com.pl>
2024-04-12 13:38:11 +0200

    deleted form

--- src/js/script.js
+++ src/js/script.js
@@ -6,13 +6,6 @@ const portfolioElements = document.querySelectorAll(
 	".portfolio-box__element-image"
 );
 const faqs = document.querySelectorAll(".faq-container");
-const username = document.querySelector("#name");
-const email = document.querySelector("#mail");
-const subject = document.querySelector("#subject");
-const messege = document.querySelector("#messege");
-const sendBtn = document.querySelector(".send");
-const clearBtn = document.querySelector(".clear");
-const popup = document.querySelector(".popup");
 
 const handleNav = () => {
 	mobileNav.classList.toggle("show-mobile-nav");
@@ -80,28 +73,6 @@ const handleFaq = (event) => {
 	clickedFaq.classList.toggle("active");
 };
 
-const showError = (input, messege) => {
-	const formBox = input.parentElement;
-	const errorMsg = formBox.querySelector(".error-text");
-
-	formBox.classList.add("error");
-};
-
-const clearError = (input) => {
-	const formBox = input.parentElement;
-	formBox.classList.remove("error");
-};
-
-const checkForm = (input) => {
-	input.forEach((el) => {
-		if (el.value === "") {
-			showError(el, el.placeholder);
-		} else {
-			clearError(el);
-		}
-	});
-};
-
 burgerBtn.addEventListener("click", handleNav);
 mobileNavItems.forEach((item) => {
 	item.addEventListener("click", closeNav);
@@ -111,16 +82,4 @@ portfolioElements.forEach((element) => {
 });
 faqs.forEach((faq) => {
 	faq.addEventListener("click", handleFaq);
-});
-clearBtn.addEventListener("click", (e) => {
-	e.preventDefault();
-
-	[username, email, subject, messege].forEach((el) => {
-		el.value = "";
-	});
-});
-sendBtn.addEventListener("click", (e) => {
-	e.preventDefault();
-
-	checkForm([username, email, subject, messege]);
 });
